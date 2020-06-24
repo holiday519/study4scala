080f96463768b5b4d91ba4ec5fe835b7c8e112ff
zhengyi <zheng.yi@kuyun.com>
2020-06-25 01:46:51 +0800

    submit

--- src/main/scala/com/ee/function/PartialFunction.scala
+++ src/main/scala/com/ee/function/PartialFunction.scala
@@ -5,20 +5,24 @@ package com.ee.function
  */
 object PartialFunction {
   def main(args: Array[String]): Unit = {
-    // 偏函数
-    def sum1(a: Int, b: Int) = a + b
-    def sum2(a: Int)(b: Int) = a + b
-    def sum3(a: Int) = (b: Int) => a + b
-    // 
-    val sum1_a_1 = sum1(1, _: Int)
-    val sum2_a_1 = sum2(1)(_: Int)
-    val sum3_a_1 = sum3(1)
-    // _代表所有参数未赋值（其实就是将该方法赋值给变量，不写这个就会报错）
-    val sum1_ = sum1 _
-    //
-    println(sum1_a_1(2))
-    println(sum2_a_1(3))
-    println(sum3_a_1(4))
+//    val partial = one orElse two orElse wildcard
+//    print(partial(1))
+    
+    val partial = one andThen firstWord
+    print(partial(1))
   }
   
+  def one: PartialFunction[Int, String] = {
+    case 1 => "one"
+  }
+  def two: PartialFunction[Int, String] = {
+    case 2 => "two"
+  }
+  def wildcard: PartialFunction[Int, String] = {
+    case _ => "something else"
+  }
+  
+  def firstWord: PartialFunction[String, String] = {
+    case "one" => "o"
+  }
 }
